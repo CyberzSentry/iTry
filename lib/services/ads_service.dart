d7ea5913de9d1d09abd985e15d8441b9ecd60f79
Maksym Brzęczek <maksym.brzeczek@gmail.com>
2020-04-29 13:10:05 +0200

    Aded targeting to ads

--- lib/services/ads_service.dart
+++ lib/services/ads_service.dart
@@ -6,24 +6,36 @@ class AdsService {
   AdsService._() {
     FirebaseAdMob.instance.initialize(appId: getAppId());
     _bannerUnitId = getBannerAdUnitId();
-    
+    _interstitialUnitId = getInterstitialAdUnitId();
   }
 
   static final AdsService _instance = AdsService._();
 
   String _bannerUnitId;
+  String _interstitialUnitId;
 
   BannerAd _banner;
+
+  InterstitialAd _interstitialAd;
+
+  static const MobileAdTargetingInfo _targetingInfo = MobileAdTargetingInfo(
+      keywords: <String>[
+        'self improvement',
+        'improvement',
+        'gym',
+        'suplements'
+      ]);
 
   factory AdsService() {
     return _instance;
   }
 
   void showBanner() {
-    if(_banner == null){
+    if (_banner == null) {
       _banner = BannerAd(
           adUnitId: _bannerUnitId,
           size: AdSize.banner,
+          targetingInfo: _targetingInfo,
           listener: (MobileAdEvent event) {
             print("BannerAd $event");
           });
@@ -35,6 +47,25 @@ class AdsService {
 
   void hideBanner() async {
     await _banner?.dispose();
+    _banner = null;
+  }
+
+  void showInterstitial() {
+    if (_interstitialAd == null) {
+      _interstitialAd = InterstitialAd(
+          adUnitId: _interstitialUnitId,
+          targetingInfo: _targetingInfo,
+          listener: (MobileAdEvent event) {
+            print("BannerAd $event");
+          });
+      _banner
+        ..load()
+        ..show();
+    }
+  }
+
+  void hideInterstitial() async {
+    await _interstitialAd?.dispose();
     _banner = null;
   }
 
@@ -52,6 +83,15 @@ class AdsService {
       return "ca-app-pub-2917376840034915/2454574389";
     } else if (Platform.isAndroid) {
       return "ca-app-pub-2917376840034915/7944852012";
+    }
+    return null;
+  }
+
+  static String getInterstitialAdUnitId() {
+    if (Platform.isIOS) {
+      return "ca-app-pub-2917376840034915/5945051233";
+    } else if (Platform.isAndroid) {
+      return "ca-app-pub-2917376840034915/9939657632";
     }
     return null;
   }
