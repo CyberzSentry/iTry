431b9dddab5fd0dfbc24526222c4387b7ba63887
Maksym Brzęczek <maksym.brzeczek@gmail.com>
2020-06-17 09:23:55 +0200

    Updated experiment page

--- lib/pages/experiments/experiment_page.dart
+++ lib/pages/experiments/experiment_page.dart
@@ -1,10 +1,33 @@
+import 'package:charts_flutter/flutter.dart' as chart;
 import 'package:flutter/material.dart';
 import 'package:itry/database/models/experiments/dose.dart';
 import 'package:itry/database/models/experiments/experiment.dart';
 import 'package:itry/pages/experiments/add_experiment_page.dart';
+import 'package:itry/pages/tests/acuity_contrast_test_page.dart';
+import 'package:itry/pages/tests/anixety_survey_page.dart';
+import 'package:itry/pages/tests/chronic_pain_survey_page.dart';
+import 'package:itry/pages/tests/creativity_productivity_survey_page.dart';
+import 'package:itry/pages/tests/creativity_productivity_test_page.dart';
+import 'package:itry/pages/tests/depression_survey_page.dart';
+import 'package:itry/pages/tests/finger_tapping_test_page.dart';
+import 'package:itry/pages/tests/pavsat_test_page.dart';
+import 'package:itry/pages/tests/spatial_memory_test_page.dart';
+import 'package:itry/pages/tests/stress_survey_page.dart';
 import 'package:itry/services/ads_service.dart';
 import 'package:itry/services/experiments/dose_service.dart';
 import 'package:itry/services/experiments/experiment_service.dart';
+import 'package:itry/services/tests/acuity_contrast_test_service.dart';
+import 'package:itry/services/tests/anxiety_survey_service.dart';
+import 'package:itry/services/tests/chronic_pain_survey_service.dart';
+import 'package:itry/services/tests/creativity_productivity_survey_service.dart';
+import 'package:itry/services/tests/creativity_productivity_test_service.dart';
+import 'package:itry/services/tests/depression_survey_service.dart';
+import 'package:itry/services/tests/finger_tapping_test_service.dart';
+import 'package:itry/services/tests/pavsat_test_service.dart';
+import 'package:itry/services/tests/spatial_memory_test_service.dart';
+import 'package:itry/services/tests/stress_survey_service.dart';
+import 'package:itry/services/tests/test_service_interface.dart';
+import 'package:tuple/tuple.dart';
 
 class ExperimentPage extends StatefulWidget {
   static const String routeName = '/experiment';
@@ -19,13 +42,130 @@ class ExperimentPage extends StatefulWidget {
 
 class _ExperimentPageState extends State<ExperimentPage> {
   _ExperimentPageState(int experimentId) {
-    _experimentFuture = _experimentService.getSingle(experimentId);
+    _id = experimentId;
   }
+
+  List<Tuple4<TestServiceInterface, String, MaterialColor, chart.Color>>
+      graphElements = [
+    Tuple4<TestServiceInterface, String, MaterialColor, chart.Color>(
+        FingerTappingTestService(),
+        FingerTappingTestPage.title,
+        Colors.blue,
+        chart.MaterialPalette.blue.shadeDefault),
+    Tuple4<TestServiceInterface, String, MaterialColor, chart.Color>(
+        CreativityProductivitySurveyService(),
+        CreativityProductivitySurveyPage.title,
+        Colors.purple,
+        chart.MaterialPalette.purple.shadeDefault),
+    Tuple4<TestServiceInterface, String, MaterialColor, chart.Color>(
+        CreativityProductivityTestService(),
+        CreativityProductivityTestPage.title,
+        Colors.red,
+        chart.MaterialPalette.red.shadeDefault),
+    Tuple4<TestServiceInterface, String, MaterialColor, chart.Color>(
+        SpatialMemoryTestService(),
+        SpatialMemoryTestPage.title,
+        Colors.lime,
+        chart.MaterialPalette.lime.shadeDefault),
+    Tuple4<TestServiceInterface, String, MaterialColor, chart.Color>(
+        DepressionSurveyService(),
+        DepressionSurveyPage.title,
+        Colors.green,
+        chart.MaterialPalette.green.shadeDefault),
+    Tuple4<TestServiceInterface, String, MaterialColor, chart.Color>(
+        StressSurveyService(),
+        StressSurveyPage.title,
+        Colors.cyan,
+        chart.MaterialPalette.cyan.shadeDefault),
+    Tuple4<TestServiceInterface, String, MaterialColor, chart.Color>(
+        AnxietySurveyService(),
+        AnxietySurveyPage.title,
+        Colors.teal,
+        chart.MaterialPalette.teal.shadeDefault),
+    Tuple4<TestServiceInterface, String, MaterialColor, chart.Color>(
+        AcuityContrastTestService(),
+        AcuityContrastTestPage.title,
+        Colors.pink,
+        chart.MaterialPalette.pink.shadeDefault),
+    Tuple4<TestServiceInterface, String, MaterialColor, chart.Color>(
+        PavsatTestService(),
+        PavsatTestPage.title,
+        Colors.deepOrange,
+        chart.MaterialPalette.deepOrange.shadeDefault),
+    Tuple4<TestServiceInterface, String, MaterialColor, chart.Color>(
+        ChronicPainSurveyService(),
+        ChronicPainSurveyPage.title,
+        Colors.indigo,
+        chart.MaterialPalette.indigo.shadeDefault),
+  ];
+
+  int _id;
   ExperimentService _experimentService = ExperimentService();
   DoseService _doseService = DoseService();
-  Future<Experiment> _experimentFuture;
   Experiment _experiment;
   List<Dose> _doses;
+  chart.TimeSeriesChart _chart;
+
+  Future<bool> _experimentData() async {
+    _experiment = await _experimentService.getSingle(_id);
+    _doses = await _doseService.getAllFromExperiment(_experiment.id);
+    _doses.sort((dose1, dose2) {
+      return dose1.date.compareTo(dose2.date);
+    });
+    return true;
+  }
+
+  Future<bool> _raportData() async {
+    final doses = <TimeSeriesElement>[];
+    for (var dose in _doses) {
+      doses.add(TimeSeriesElement(date: dose.date));
+    }
+
+    List<chart.Series<TimeSeriesElement, DateTime>> series = [
+      chart.Series<TimeSeriesElement, DateTime>(
+        id: 'Doses',
+        colorFn: (_, __) => chart.MaterialPalette.blue.shadeDefault,
+        domainFn: (TimeSeriesElement dose, __) => dose.date,
+        measureFn: (_, __) => null,
+        data: doses,
+      )
+        // Configure our custom symbol annotation renderer for this series.
+        // ..setAttribute(chart.rendererIdKey, 'customSymbolAnnotation')
+        // Optional radius for the annotation shape. If not specified, this will
+        // default to the same radius as the points.
+        ..setAttribute(chart.boundsLineRadiusPxKey, 3.5),
+    ];
+
+    final data1 = <TimeSeriesElement>[];
+    var data = await graphElements[0].item1.getAll();
+
+    for (var it in data) {
+      data1.add(TimeSeriesElement(date: it.date, value: it.percentageScore));
+    }
+
+    series.add(chart.Series<TimeSeriesElement, DateTime>(
+      id: 'Data1',
+      colorFn: (_, __) => chart.MaterialPalette.blue.shadeDefault,
+      domainFn: (TimeSeriesElement data, __) => data.date,
+      measureFn: (TimeSeriesElement data, __) => data.value,
+      domainLowerBoundFn: (_, __) => null,
+      domainUpperBoundFn: (_, __) => null,
+      data: data1,
+    ));
+
+    _chart = chart.TimeSeriesChart(
+      series,
+      animate: true,
+      customSeriesRenderers: [
+        chart.SymbolAnnotationRendererConfig(
+            // ID used to link series to this renderer.
+            customRendererId: 'customSymbolAnnotation')
+      ],
+      // dateTimeFactory: const chart.LocalDateTimeFactory(),
+    );
+
+    return true;
+  }
 
   void _deleteExperiment() {
     showDialog(
@@ -155,12 +295,10 @@ class _ExperimentPageState extends State<ExperimentPage> {
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 3,
-      child: FutureBuilder<Experiment>(
-        future: _experimentFuture,
+      child: FutureBuilder<bool>(
+        future: _experimentData(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            _experiment = snapshot.data;
-
             var doses = <Widget>[];
             doses.add(
               ListTile(
@@ -169,6 +307,57 @@ class _ExperimentPageState extends State<ExperimentPage> {
                 onTap: _addDose,
               ),
             );
+
+            var output = <Widget>[];
+            output.add(
+              ListTile(
+                title: Text('Add dose'),
+                trailing: Icon(Icons.add),
+                onTap: _addDose,
+              ),
+            );
+
+            for (var dose in _doses) {
+              output.add(Dismissible(
+                  background: Container(color: Colors.red),
+                  confirmDismiss: (direction) {
+                    return showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text('Delete'),
+                          content: Text(
+                              'Are you sure you want to delete this value?'),
+                          actions: <Widget>[
+                            FlatButton(
+                              color: Colors.red,
+                              onPressed: () => Navigator.of(context).pop(true),
+                              child: Text('Continue'),
+                            ),
+                            FlatButton(
+                              onPressed: () => Navigator.of(context).pop(false),
+                              child: Text('Abort'),
+                            )
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  onDismissed: (direction) async {
+                    await _doseService.delete(dose.id);
+                    _doses.remove(dose);
+                  },
+                  key: Key(dose.id.toString()),
+                  child: ListTile(
+                    title: Text(
+                      dose.value.toString(),
+                    ),
+                    subtitle: Text(
+                      dose.date.toString().substring(0, 16),
+                    ),
+                    trailing: Text(_experiment.unit.stringValue()),
+                  )));
+            }
 
             return Scaffold(
               appBar: AppBar(
@@ -194,78 +383,22 @@ class _ExperimentPageState extends State<ExperimentPage> {
                 padding: EdgeInsets.fromLTRB(0, 0, 0, 49),
                 child: TabBarView(
                   children: [
-                    Container(),
-                    FutureBuilder<List<Dose>>(
-                      future:
-                          DoseService().getAllFromExperiment(_experiment.id),
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          _doses = snapshot.data;
-
-                          var output = <Widget>[];
-                          output.add(
-                            ListTile(
-                              title: Text('Add dose'),
-                              trailing: Icon(Icons.add),
-                              onTap: _addDose,
-                            ),
-                          );
-
-                          for (var dose in _doses) {
-                            output.add(Dismissible(
-                                background: Container(color: Colors.red),
-                                confirmDismiss: (direction) {
-                                  return showDialog(
-                                    context: context,
-                                    builder: (context) {
-                                      return AlertDialog(
-                                        title: Text('Delete'),
-                                        content: Text(
-                                            'Are you sure you want to delete this value?'),
-                                        actions: <Widget>[
-                                          FlatButton(
-                                            color: Colors.red,
-                                            onPressed: () =>
-                                                Navigator.of(context).pop(true),
-                                            child: Text('Continue'),
-                                          ),
-                                          FlatButton(
-                                            onPressed: () =>
-                                                Navigator.of(context)
-                                                    .pop(false),
-                                            child: Text('Abort'),
-                                          )
-                                        ],
-                                      );
-                                    },
-                                  );
-                                },
-                                onDismissed: (direction) async {
-                                  await _doseService.delete(dose.id);
-                                  _doses.remove(dose);
-                                },
-                                key: Key(dose.id.toString()),
-                                child: ListTile(
-                                  title: Text(
-                                    dose.value.toString(),
-                                  ),
-                                  subtitle: Text(
-                                    dose.date.toString().substring(0, 16),
-                                  ),
-                                  trailing:
-                                      Text(_experiment.unit.stringValue()),
-                                )));
-                          }
-
-                          return ListView(
-                            children: output,
-                          );
-                        } else {
-                          return Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        }
-                      },
+                    _doses.length > 0
+                        ? FutureBuilder(
+                            future: _raportData(),
+                            builder: (context, snapshot) {
+                              if (snapshot.hasData) {
+                                return Expanded(child: _chart);
+                              } else {
+                                return Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              }
+                            },
+                          )
+                        : Center(child: Text("Add doses to start experiment.")),
+                    ListView(
+                      children: output,
                     ),
                     ListView(
                       children: <Widget>[
@@ -313,8 +446,6 @@ class _ExperimentPageState extends State<ExperimentPage> {
                                 ),
                               )
                                   .then((value) {
-                                _experimentFuture = _experimentService
-                                    .getSingle(_experiment.id);
                                 setState(() {});
                               }),
                               child: Text('Modify'),
@@ -346,4 +477,10 @@ class _ExperimentPageState extends State<ExperimentPage> {
       ),
     );
   }
+}
+
+class TimeSeriesElement {
+  DateTime date;
+  double value;
+  TimeSeriesElement({this.date, this.value});
 }
